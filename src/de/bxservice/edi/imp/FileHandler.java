abc72b05c3e72ea54c062785371284aea05f21df
Diego Ruiz <ruizg.diego@gmail.com>
2022-02-15 22:40:26 +0100

    Add headers

--- src/de/bxservice/edi/imp/FileHandler.java
+++ src/de/bxservice/edi/imp/FileHandler.java
@@ -1,3 +1,27 @@
+/**********************************************************************
+ * This file is part of iDempiere ERP Open Source                      *
+ * http://www.idempiere.org                                            *
+ *                                                                     *
+ * Copyright (C) Contributors                                          *
+ *                                                                     *
+ * This program is free software; you can redistribute it and/or       *
+ * modify it under the terms of the GNU General Public License         *
+ * as published by the Free Software Foundation; either version 2      *
+ * of the License, or (at your option) any later version.              *
+ *                                                                     *
+ * This program is distributed in the hope that it will be useful,     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ * GNU General Public License for more details.                        *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with this program; if not, write to the Free Software         *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+ * MA 02110-1301, USA.                                                 *
+ *                                                                     *
+ * Contributors:                                                       *
+ * - Diego Ruiz - BX Service GmbH                                      *
+ **********************************************************************/
 package de.bxservice.edi.imp;
 
 import java.util.List;
@@ -66,7 +90,7 @@ public class FileHandler {
 	}
 	
 	private void parseMessages() {
-		parseMessageHeader(); // while new order
+		parseMessageHeader(); // while new order. TODO: Same as details with UNH+MessageNumber
 		parseMessageDetail();
 		parseMessageSummary(); //every new order
 	}
@@ -108,6 +132,10 @@ public class FileHandler {
 	}
 	
 	private void parseInterchageFooter() {
-		//Close properly, check file is correct
+		//TODO: Close properly, check file is correct
+		MEDISection trailerSection = ediFormat.getInterchangeTrailer();
+		List<ValueNamePair> columnNameValues = ediLinesHandler.parseSection(trailerSection);
+		//TODO: Save 92000000000001 Nachrichtreferenz to check if it is the same as the last line for consistency
+		messageCreator.appendValues(columnNameValues);
 	}
 }
